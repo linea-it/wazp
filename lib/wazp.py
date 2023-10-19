03b7ece7e424505e125ff5cb3429951d92244735
Christophe Benoist <benoist@oca.eu>
2023-10-19 11:14:53 +0200

    rm the fct run_wazp_tile - substituted by wazp_tile.py

--- lib/wazp.py
+++ lib/wazp.py
@@ -2017,66 +2017,6 @@ def wazp_tile(admin, tile_specs,
     return 
 
 
-def run_wazp_tile(config, dconfig, thread_id):
-    # read config file
-    with open(config) as fstream:
-        param_cfg = yaml.safe_load(fstream)
-    with open(dconfig) as fstream:
-        param_data = yaml.safe_load(fstream)
-
-    survey, ref_filter  = param_cfg['survey'], param_cfg['ref_filter']
-    maglim = param_cfg['maglim_det']
-    galcat = param_data['galcat'][survey]
-    clcat = param_cfg['clcat']
-    out_paths = param_cfg['out_paths']
-    admin = param_cfg['admin']
-    footprint = param_data['footprint'][survey]
-    zp_metrics = param_data['zp_metrics'][survey][ref_filter]
-    magstar_file = param_data['magstar_file'][survey][ref_filter]
-    wazp_cfg = param_cfg['wazp_cfg']
-
-    workdir = out_paths['workdir']
-    all_tiles = read_FitsCat(
-        os.path.join(
-            workdir, 'sky_partition',
-            admin['tiling']['tiles_filename'])
-    )
-    idt = np.argwhere(all_tiles['thread_id']==int(thread_id)).T[0]    
-    tiles_specs = all_tiles[idt]
-    hpix_tile_lists = np.load(
-        os.path.join(
-            workdir, 'sky_partition', 
-            admin['tiling']['tiles_npy']
-        ), 
-        allow_pickle=True
-    )[idt]
-    hpix_core_lists = np.load(
-        os.path.join(
-            workdir, 'sky_partition', 
-            admin['tiling']['sky_partition_npy']
-        ), 
-        allow_pickle=True
-    )[idt]
-
-    print ('THREAD ', int(thread_id))
-
-    for it in range(0, len(tiles_specs)):
-
-        tile_specs = create_tile_specs(
-            tiles_specs[it], admin, 
-            hpix_core_lists[it], hpix_tile_lists[it],
-            None, None
-        )
-        wazp_tile(
-            admin, tile_specs, 
-            galcat, footprint, 
-            zp_metrics, magstar_file, maglim,
-            wazp_cfg, clcat, param_cfg['cosmo_params'], 
-            out_paths, param_cfg['verbose'] ) 
-
-    return
-
-
 def tiles_with_clusters(out_paths, all_tiles):
     flag = np.zeros(len(all_tiles))
     for it in range(0, len(all_tiles)):
